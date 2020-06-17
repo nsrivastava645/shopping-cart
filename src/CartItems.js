c2bfa9e2ef674dc016b90f0d8c03af2bbf17265e
nsrivastava645 <nsrivastava645@gmail.com>
2020-06-17 16:39:49 +0530

    add increase quantity function

--- src/CartItems.js
+++ src/CartItems.js
@@ -1,19 +1,51 @@
 import React from 'react';
 
 class CartItems extends React.Component {
+   
+    constructor(){
+         // you need to call super constructor because t extends component class abd its constructor needs calling
+        super();
+        this.state = {
+            price: 999,
+            title: 'Phone',
+            qantity: 1,
+            img: '',
+        }
+        //this.increaseQuantity = this.increaseQuantity.bind(this);
+    }
+    increaseQuantity = ()=>{
+        console.log('this.state', this.state);
+    }
     
     render(){
+        const {price, title, qantity} = this.state;
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image}/>
                 </div>
                 <div className="right-block">
-                    <div style={{ fontSize: 25}}>Phone</div>
-                    <div style={{ color: 'lightgrey'}}>Rs 1000</div>
-                    <div style={{ color: 'lightgrey'}}>Quantity: 1</div>
+                    <div style={{ fontSize: 25}}>{title}</div>
+                    <div style={{ color: 'lightgrey'}}>Rs: {price}</div>
+                    <div style={{ color: 'lightgrey'}}>Quantity: {qantity}</div>
                     <div className="cart-items-actions">
                         {/* Action buttons */}
+                        <img 
+                        alt="increase" 
+                        className="action-icons" 
+                        src="https://image.flaticon.com/icons/svg/992/992651.svg"
+                        onClick={this.increaseQuantity}
+                        />
+                        <img 
+                        alt="decrease" 
+                        className="action-icons" 
+                        src="https://image.flaticon.com/icons/svg/659/659892.svg"
+                        />
+                        <img 
+                        alt="delete" 
+                        className="action-icons" 
+                        src="https://image.flaticon.com/icons/svg/1345/1345823.svg"
+                        />
                     </div>
                 </div>
             </div>
@@ -34,7 +66,6 @@ const styles = {
         width: 110,
         borderRadius: 4,
         backgroundColor: 'grey'
-
     }
 }
 export default CartItems;
